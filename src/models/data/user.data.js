6a8b8226970963cd5cc5a9d87ea1a8e08183b1fb
wale2009 <adewaleadebayo202@gmail.com>
2025-03-27 03:14:31 +0100

    compress and limit endpoint

--- src/models/data/user.data.js
+++ src/models/data/user.data.js
@@ -1,27 +1,3 @@
-const users = [
-  {
-    id: 1,
-    name: "John Doe",
-    email: "stuff@gmail.com",
-    city: "Lagos",
-    country: "Nigeria",
-    gender: "Male",
-  },
-  {
-    id: 2,
-    name: "Jane Doe",
-    email: "jane.doe@example.com",
-    city: "Nairobi",
-    country: "Kenya",
-    gender: "Female",
-   },
-  {
-    id: 3,
-    name: "John Doe",
-    email: "john.doe@example.com",
-    city: "Lagos",
-    country: "Nigeria",
-  }
-];
+const users = [];
 
 export default users;
